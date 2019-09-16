9e4e09ee2c2251458887aad1789bffd0eff5b988
Peter Bushnell <bushsolo@gmail.com>
2019-09-16 10:21:31 +0100

    Build, version, dist and icons

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"org.slice.Slice-Qt";
     } else {
         return [self __bundleIdentifier];
     }
